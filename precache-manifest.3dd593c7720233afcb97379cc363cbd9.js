25ae8454583516ea1d80d86e2b8ae937f9cc9b37
Daniel AHMED <daniel.ahmedinfo@gmail.com>
2018-11-25 13:27:33 +0100

    Updates

---
+++ precache-manifest.3dd593c7720233afcb97379cc363cbd9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "04529d515f197bef9fe3",
+    "url": "/d-ahmed/test-foglet-template/static/js/runtime~main.04529d51.js"
+  },
+  {
+    "revision": "78d0657b8a11bb9c4fc8",
+    "url": "/d-ahmed/test-foglet-template/static/js/main.78d0657b.chunk.js"
+  },
+  {
+    "revision": "ae55720de97069b8542b",
+    "url": "/d-ahmed/test-foglet-template/static/js/1.ae55720d.chunk.js"
+  },
+  {
+    "revision": "78d0657b8a11bb9c4fc8",
+    "url": "/d-ahmed/test-foglet-template/static/css/main.651c844d.chunk.css"
+  },
+  {
+    "revision": "1035e52d7eb97ceb42090f649fea6656",
+    "url": "/d-ahmed/test-foglet-template/index.html"
+  }
+];
