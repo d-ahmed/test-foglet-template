1d8dace1b2c29d36a10261a90ada269329adb479
Daniel AHMED <daniel.ahmedinfo@gmail.com>
2018-11-25 01:35:43 +0100

    Add docs

---
+++ docs/precache-manifest.fe48f05987391301c61040d97ee758fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0c7d8a40576e9069eff7",
+    "url": "/static/js/main.0c7d8a40.chunk.js"
+  },
+  {
+    "revision": "62cb5f61037e705430e5",
+    "url": "/static/js/1.62cb5f61.chunk.js"
+  },
+  {
+    "revision": "0c7d8a40576e9069eff7",
+    "url": "/static/css/main.c2ec639b.chunk.css"
+  },
+  {
+    "revision": "33df116c1133d19794288702db9c79c4",
+    "url": "/index.html"
+  }
+];
